015f41ba43114228f22dd46ab2db10c5cafeb978
Mike Perrwood <mike.perrwood@gmail.com>
2023-09-16 15:33:28 +0300

    add EvenGame (partityCheck method)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
+import hexlet.code.games.Even;
+
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Cli.welcome();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n" +
+                           "2 - Even\n" +
+                           "0 - Exit\n");
+        Scanner choiceGame = new Scanner(System.in);
+        System.out.print("Your choice: ");
+        int numberOfGame = choiceGame.nextInt();
+        switch (numberOfGame) {
+            case 1:
+                Cli.welcome();
+                break;
+            case 2:
+                Even.parityCheck();
+                break;
+            case 0:
+                break;
+        }
     }
 }
