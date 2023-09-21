a3e307ee774157c777e77ad7c8c5a4cb3dec29b2
Mike Perrwood <mike.perrwood@gmail.com>
2023-09-21 21:01:31 +0300

    added call to 'calc' method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,7 @@
 package hexlet.code;
+import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
 
 import java.util.Scanner;
 
@@ -8,19 +10,28 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet\n" +
                            "2 - Even\n" +
+                           "3 - Calc\n" +
+                           "4 - GCD\n" +
                            "0 - Exit\n");
-        Scanner choiceGame = new Scanner(System.in);
         System.out.print("Your choice: ");
+        Scanner choiceGame = new Scanner(System.in);
         int numberOfGame = choiceGame.nextInt();
         switch (numberOfGame) {
             case 1:
                 Cli.welcome();
                 break;
             case 2:
-                Even.parityCheck();
+                Even.runGame();
+                break;
+            case 3:
+                Calc.runGame();
+                break;
+            case 4:
+                GCD.runGame();
                 break;
             case 0:
                 break;
+            default: System.out.println("Operation selection error.");
         }
     }
 }
