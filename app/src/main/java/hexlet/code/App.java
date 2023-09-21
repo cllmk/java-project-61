5e7e7937cbf375a3a78502a8a8bbf4cb4e2ca491
Mike Perrwood <mike.perrwood@gmail.com>
2023-09-22 00:44:04 +0300

    fix output of game number

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n" +
-                           "2 - Even\n" +
-                           "3 - Calc\n" +
-                           "4 - GCD\n" +
-                           "5 - Progression\n" +
-                           "6 - Prime\n" +
-                           "0 - Exit\n");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         Scanner choiceGame = new Scanner(System.in);
         int numberOfGame = choiceGame.nextInt();
