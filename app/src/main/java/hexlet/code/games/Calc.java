95a2b04c5340546db331b55ff7980dafe662e60b
Mike Perrwood <mike.perrwood@gmail.com>
2023-09-21 21:04:39 +0300

    add game Calc.java

---
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -0,0 +1,36 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import static hexlet.code.Engine.COUNT_ROUNDS;
+
+public class Calc {
+    private static String RULES_GAME = "What is the result of the expression?";
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 100;
+    private static final String[] OPERATORS = {" + ", " - ", " * "};
+
+    public static void runGame() {
+        String[][] rightAnswers = generateExpressionsAndRightAnswers();
+        Engine.runGame(RULES_GAME, rightAnswers);
+    }
+    private static String[][] generateExpressionsAndRightAnswers() {
+        String[][] expressionsAndAnswer = new String[COUNT_ROUNDS][2];
+        for (int i = 0; i < COUNT_ROUNDS; i++) {
+            int number1 = Engine.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
+            int number2 = Engine.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
+            int operator = Engine.getRandomNumber(0, OPERATORS.length);
+            expressionsAndAnswer[i][0] = number1 + OPERATORS[operator] + number2;
+            expressionsAndAnswer[i][1] = String.valueOf(calculate(number1, number2, OPERATORS[operator]));
+        }
+        return expressionsAndAnswer;
+    }
+    public static int calculate(int num1, int num2, String operator) {
+        return switch (operator) {
+            case " + " -> num1 + num2;
+            case " - " -> num1 - num2;
+            case " * " -> num1 * num2;
+            default -> throw new IllegalStateException("Unexpected value: " + operator);
+        };
+    }
+}
