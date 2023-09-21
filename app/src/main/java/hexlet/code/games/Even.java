01d1aa63e165cb7ef6d50941456a06b730285d2a
Mike Perrwood <mike.perrwood@gmail.com>
2023-09-21 21:08:14 +0300

    fix file for the Engine.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,13 +1,27 @@
 package hexlet.code.games;
 
-import hexlet.code.LogicsEven;
+import hexlet.code.Engine;
+import static hexlet.code.Engine.COUNT_ROUNDS;
+
+import java.util.Scanner;
 
 public class Even {
-    public static void parityCheck() {
-        LogicsEven.welcome();
-        LogicsEven.setCountCycle(3);
-        LogicsEven.setMinMaxRandom(0, 100);
-        LogicsEven.run();
+    private static String RULES_GAME = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 1000;
+    private static String[][] generateExpressionsAndRightAnswers(){
+        String[][] expressionsAndAnswer = new String[COUNT_ROUNDS][2];
+        for (int i = 0; i < COUNT_ROUNDS; i++) {
+            int number = Engine.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
+            String answer = number % 2 == 0 ? "yes" : "no";
+            expressionsAndAnswer[i][0] = String.valueOf(number);
+            expressionsAndAnswer[i][1] = answer;
+        }
+        return expressionsAndAnswer;
+    }
+    public static void runGame() {
+        String[][] rightAnswers = generateExpressionsAndRightAnswers();
+        Engine.runGame(RULES_GAME, rightAnswers);
     }
 }
 
