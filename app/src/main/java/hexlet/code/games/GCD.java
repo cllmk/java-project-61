545931345313d78ad36b0292c906a3cc0c9b9137
Mike Perrwood <mike.perrwood@gmail.com>
2023-09-21 21:08:46 +0300

    add game GCD.java

---
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,35 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import static hexlet.code.Engine.COUNT_ROUNDS;
+
+public class GCD {
+    private static String RULES_GAME = "Find the greatest common divisor of given numbers.";
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 100;
+    private static String[][] generateExpressionsAndRightAnswers() {
+        String[][] expressionsAndAnswer = new String[COUNT_ROUNDS][2];
+        for (int i = 0; i < COUNT_ROUNDS; i++) {
+            int number1 = Engine.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
+            int number2 = Engine.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
+            expressionsAndAnswer[i][0] = number1 + " " + number2;
+            int tempMod;
+            int maxDiv = Math.max(number1, number2);
+            int minDiv = Math.min(number1, number2);
+            if (minDiv == 0) {
+                expressionsAndAnswer[i][1] = minDiv + "";
+            } else do {
+                tempMod = maxDiv % minDiv;
+                maxDiv = minDiv;
+                minDiv = tempMod;
+                expressionsAndAnswer[i][1] = minDiv + "";
+            } while (maxDiv % minDiv != 0);
+        }
+        return expressionsAndAnswer;
+    }
+    public static void runGame() {
+        String[][] rightAnswers = generateExpressionsAndRightAnswers();
+        Engine.runGame(RULES_GAME, rightAnswers);
+    }
+}
