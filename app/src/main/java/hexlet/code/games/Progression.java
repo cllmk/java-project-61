ff2b49cbf151f64e1047cf09485bc4ae4882c1a9
Mike Perrwood <mike.perrwood@gmail.com>
2023-09-26 01:13:57 +0300

    add separate method generateProgression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,12 +1,14 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
 import java.util.Arrays;
 
 import static hexlet.code.Engine.COUNT_ROUNDS;
 
 public class Progression {
+
     private static final String RULES_GAME = "What number is missing in the progression?";
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 100;
@@ -14,30 +16,38 @@ public class Progression {
     private static final int MAX_LENGTH_PROGRESSION = 10;
     public static final int MIN_STEP_PROGRESSION = 1;
     public static final int MAX_STEP_PROGRESSION = 5;
+
     private static String[][] generateExpressionsAndRightAnswers() {
         String[][] expressionsAndAnswer = new String[COUNT_ROUNDS][2];
         for (int i = 0; i < COUNT_ROUNDS; i++) {
-            int lengthProgression = Engine.getRandomNumber(MIN_LENGTH_PROGRESSION, MAX_LENGTH_PROGRESSION);
-            int indexOfHiddenElement = Engine.getRandomNumber(0, lengthProgression - 1);
-            String hiddenElement = null;
-            String[] progression = new String[lengthProgression];
-            int preElementOfProgression = Engine.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
-            int stepProgression = Engine.getRandomNumber(MIN_STEP_PROGRESSION, MAX_STEP_PROGRESSION);
-            for (int j = 0; j < lengthProgression; j++) {
-                progression[j] = String.valueOf(preElementOfProgression + stepProgression);
-                preElementOfProgression = preElementOfProgression + stepProgression;
-                if (j == indexOfHiddenElement) {
-                    hiddenElement = progression[j];
-                    progression[j] = "..";
-                }
-            }
+            int lengthProgression = Utils.getRandomNumber(MIN_LENGTH_PROGRESSION, MAX_LENGTH_PROGRESSION);
+            int stepProgression = Utils.getRandomNumber(MIN_STEP_PROGRESSION, MAX_STEP_PROGRESSION);
+            int preElementOfProgression = Utils.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
+            String[][] progressionAndHiddenElement = generateProgression(preElementOfProgression, stepProgression, lengthProgression);
+            String[] progression = progressionAndHiddenElement[0];
+            String hiddenElement = progressionAndHiddenElement[1][0];
             expressionsAndAnswer[i][0] = Arrays.toString(progression).replaceAll(",|\\[|]", "");
             expressionsAndAnswer[i][1] = hiddenElement;
         }
         return expressionsAndAnswer;
     }
+
     public static void runGame() {
         String[][] rightAnswers = generateExpressionsAndRightAnswers();
         Engine.runGame(RULES_GAME, rightAnswers);
+    }
+
+    public static String[][] generateProgression(int firstNumberOfProgression, int stepProgression, int sizeOfProgression){
+        String[][] progression = new String[2][sizeOfProgression];
+        int indexOfHiddenElement = Utils.getRandomNumber(0, sizeOfProgression - 1);
+        for (int j = 0; j < sizeOfProgression; j++) {
+            progression[0][j] = String.valueOf(firstNumberOfProgression + stepProgression);
+            firstNumberOfProgression = firstNumberOfProgression + stepProgression;
+            if (j == indexOfHiddenElement) {
+                progression[1][0] = progression[0][j];   // add hiddenElement to [1][0] for processing in the Engine
+                progression[0][j] = "..";
+            }
+        }
+        return progression;
     }
 }
