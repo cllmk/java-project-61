215ecdd59c76c5039ee2edc4e876db8f2ea216f3
Mike Perrwood <mike.perrwood@gmail.com>
2023-09-26 01:04:54 +0300

    method getRandomNumber moved to a separate Class (Utils)

---
+++ app/src/main/java/hexlet/code/Utils.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import java.util.Random;
+
+public class Utils {
+
+    public static int getRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+}
