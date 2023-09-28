f1c8c15bf35b23247bbc1bc000005eba2a231279
Mike Perrwood <mike.perrwood@gmail.com>
2023-09-28 23:52:30 +0300

    fix code formatting

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,34 +4,32 @@ import java.util.Scanner;
 
 public class Engine {
 
-    private static String userName;
-
     public static final int COUNT_ROUNDS = 3;
 
     public static void runGame(String rulesGame, String[][] questionExpression) {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Scanner sc = new Scanner(System.in);
-        userName = sc.next();
+        String userName = sc.next();
         System.out.println("Hello, " + userName + "!");
         System.out.println(rulesGame);
-            for (int i = 0; i < COUNT_ROUNDS; i++) {
-                String question = questionExpression[i][0];
-                String rightAnswer = questionExpression[i][1];
-                System.out.println("Question: " + question);
-                System.out.print("Your answer: ");
-                String playerAnswer = sc.next();
-                boolean isRightAnswer = rightAnswer.equals(playerAnswer);
-                if (isRightAnswer) {
-                    System.out.println("Correct!");
-                    if (i == 2) {
-                        System.out.println("Congratulations, " + userName + "!");
-                    }
-                } else {
-                    System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'%n", playerAnswer, rightAnswer);
-                    System.out.println("Let's try again, " + userName + "!");
-                    break;
+        for (int i = 0; i < COUNT_ROUNDS; i++) {
+            String question = questionExpression[i][0];
+            String rightAnswer = questionExpression[i][1];
+            System.out.println("Question: " + question);
+            System.out.print("Your answer: ");
+            String playerAnswer = sc.next();
+            boolean isRightAnswer = rightAnswer.equals(playerAnswer);
+            if (isRightAnswer) {
+                System.out.println("Correct!");
+                if (i == 2) {
+                    System.out.println("Congratulations, " + userName + "!");
                 }
+            } else {
+                System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'%n", playerAnswer, rightAnswer);
+                System.out.println("Let's try again, " + userName + "!");
+                break;
+            }
         }
     }
 }
