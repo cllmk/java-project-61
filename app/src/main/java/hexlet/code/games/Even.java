70110c5c35bcd12f2c44751653daddc5c5c5425a
Mike Perrwood <mike.perrwood@gmail.com>
2023-09-28 23:53:08 +0300

    add new method isEven

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -9,18 +9,23 @@ public class Even {
     private static final String RULES_GAME = "Answer 'yes' if the number is even, otherwise answer 'no'.";
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 1000;
+
     private static String[][] generateExpressionsAndRightAnswers() {
         String[][] expressionsAndAnswer = new String[COUNT_ROUNDS][2];
         for (int i = 0; i < COUNT_ROUNDS; i++) {
             int number = Utils.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
-            String answer = number % 2 == 0 ? "yes" : "no";
             expressionsAndAnswer[i][0] = String.valueOf(number);
-            expressionsAndAnswer[i][1] = answer;
+            expressionsAndAnswer[i][1] = isEven(number) ? "yes" : "no";;
         }
         return expressionsAndAnswer;
     }
+
     public static void runGame() {
         String[][] rightAnswers = generateExpressionsAndRightAnswers();
         Engine.runGame(RULES_GAME, rightAnswers);
+    }
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
